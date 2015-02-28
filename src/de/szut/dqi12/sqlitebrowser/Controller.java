716f0d3d89f6e23730ccc630718a84cab06f254c
Manfred <Manfred@beast.fritz.box>
2015-02-28 13:07:44 +0100

    Properties work correctly

--- src/de/szut/dqi12/sqlitebrowser/Controller.java
+++ src/de/szut/dqi12/sqlitebrowser/Controller.java
@@ -2,7 +2,11 @@ package de.szut.dqi12.sqlitebrowser;
 
 import de.szut.dqi12.sqlitebrowser.gui.View;
 import de.szut.dqi12.sqlitebrowser.sqlite.Model;
+import de.szut.dqi12.sqlitebrowser.util.SettingsUtil;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 /**
@@ -13,11 +17,13 @@ public class Controller {
     
     private static Model model;
     private static View view;
+    public static SettingsUtil settings;
     public static Controller instance;
     
     private Controller() {
         view = View.getView();
-        model = Model.getModel();
+        model = new Model();
+        settings = new SettingsUtil();
     }
     
     public static Controller getController(){
@@ -39,5 +45,11 @@ public class Controller {
     
     public static void main(String[] args){
         Controller.getController();
+        try {
+            Controller.settings.updateProperties("jdbc.properties");
+        } catch (IOException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println(SettingsUtil.properties.getProperty(SettingsUtil.JDBC_DRIVER_NAME));
     }
 }
--- src/de/szut/dqi12/sqlitebrowser/util/SettingsUtil.java
+++ src/de/szut/dqi12/sqlitebrowser/util/SettingsUtil.java
@@ -2,13 +2,14 @@ package de.szut.dqi12.sqlitebrowser.util;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
 /**
  * Verschiedene Einstellungen für den SQLiteBrowser.
- * 
+ *
  * @author Till Schlechtweg
  */
 public class SettingsUtil {
@@ -25,59 +26,77 @@ public class SettingsUtil {
     public static String VIEW_X_POSITION = "view.x";
     public static String VIEW_Y_POSITION = "view.y";
 
-    public static String SETTINGS_FILE = "sqlitebrowser.properties";
-    
-    /**
-     * Liest die Einstellungen der @see Settings#SETTINGS_FILE ein.
-     * 
-     * @author Harm Hörnlein & Till Schlechtweg
-     * @param filename
-     * @return 0, wenn Updaten der Properties fehlgeschlagen ist und 1 wenn es erfolgreich war.
-     */
-    private int updateProperties(String filename) {
-        Properties prop = new Properties();
-        File file = new File(SettingsUtil.SETTINGS_FILE);
+    public static Properties properties = new Properties();
+
+    public SettingsUtil(){
         
-        if(!file.exists()){
-            return 0;
-        }
-        
-        try {
-            try (InputStream inputStream = getClass().getResourceAsStream(SettingsUtil.SETTINGS_FILE)) {
-                prop.load(inputStream);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return 1;
     }
     
     /**
-     *  Speichert die Properties ab.
-     * 
+     * Liest die Einstellungen der @see SettingsUtil#SETTINGS_FILE ein.
+     *
+     * @author Harm Hörnlein & Till Schlechtweg
+     * @param filename
+     * @return 0, wenn Updaten der Properties fehlgeschlagen ist und 1 wenn es
+     * erfolgreich war.
+     * @throws java.io.IOException
+     */
+    public boolean updateProperties(String filename) throws IOException {
+        File file = new File(filename);
+
+        if (!file.exists()) {
+            if(!file.createNewFile()){
+                return false;
+            }else{
+                return saveStandardProperties(filename);
+            }
+        }
+        
+        try {
+            try (InputStream inputStream = getClass().getResourceAsStream(filename)) {
+                SettingsUtil.properties.load(inputStream);
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean saveStandardProperties(String filename) {
+        SettingsUtil.properties.setProperty(SettingsUtil.JDBC_DRIVER_NAME, "org.sqlite.JDBC");
+        SettingsUtil.properties.setProperty(SettingsUtil.VIEW_HEIGHT, "600");
+        SettingsUtil.properties.setProperty(SettingsUtil.VIEW_WIDTH, "800");
+        SettingsUtil.properties.setProperty(SettingsUtil.VIEW_X_POSITION, "0");
+        SettingsUtil.properties.setProperty(SettingsUtil.VIEW_Y_POSITION, "0");
+        SettingsUtil.properties.setProperty(SettingsUtil.DB_URL, "jdbc:sqlite:.geodaten.db3");
+        SettingsUtil.properties.setProperty(SettingsUtil.DB_USER, "root");
+        SettingsUtil.properties.setProperty(SettingsUtil.DB_PASSWORD, "");
+        
+        File file = new File(filename);
+        
+         try {
+            OutputStream outputStream = new FileOutputStream(file);
+            SettingsUtil.properties.store(outputStream, "Settings for SQLLiteBrowser");
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Speichert die Properties ab.
+     *
      * @param url
      * @param user
-     * @param pass 
+     * @param pass
      */
-    public void saveProperties(String url, String user, String pass){
-        Properties prop = new Properties();
-        File file = new File(SettingsUtil.SETTINGS_FILE);
-        
-        prop.setProperty("jdbc.driver", this.JDBC_DRIVER_NAME);
-        prop.setProperty("view.height", this.VIEW_HEIGHT);
-        prop.setProperty("view.width", this.VIEW_WIDTH);
-        prop.setProperty("view.xposition", this.VIEW_X_POSITION);
-        prop.setProperty("view.yposition", this.VIEW_Y_POSITION);
-        
-        prop.setProperty("jdbc.url", this.DB_URL);
-        prop.setProperty("jdbc.user", this.DB_USER);
-        prop.setProperty("jdbc.password", this.DB_PASSWORD);
-        
-               
-        try{
+    public void saveProperties(String filename) {
+        File file = new File(filename);
+
+        try {
             OutputStream outputStream = new FileOutputStream(file);
-            prop.store(outputStream, "Settings for SQLLiteBrowser");
-        }catch(Exception e){
+            SettingsUtil.properties.store(outputStream, "Settings for SQLLiteBrowser");
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
